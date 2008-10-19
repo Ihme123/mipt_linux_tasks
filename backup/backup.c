28b0dcff1b4575e654d1c36725b2e0b6b64d5fa8
Alexander Potashev <aspotashev@gmail.com>
2008-10-19 20:39:51 +0400

    err macro

--- backup/backup.c
+++ backup/backup.c
@@ -1,17 +1,31 @@
 
-void create_backup (const char *backup, const char *source)
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define err(format, args...) \
+	fprintf (stderr, format "\n", ## args)
+
+void backup_dir (const char *backup, const char *source)
 {
-	DIR *... =  opendir
+	DIR *source_dir;
+
+	source_dir = opendir (source);
+	if (source_dir == NULL) {
+		err ("error - 0x%x", 100);
+	}
 }
 
 int main (int argc, char *argv [])
 {
+	err ("my error - 0x%x", 100);
+
 	if (argc != 3) {
-		printf ("Usage: backup <src-dir> <dest-dir>");
+		err ("Usage: backup <src-dir> <dest-dir>");
 		return 1;
 	}
 
-	create_backup (argv [2], argv [1]);
+	backup_dir (argv [2], argv [1]);
 
 	return 0;
 }
