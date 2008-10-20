e672837f46d5d7a158b5cf554c259cbd3379e102
Alexander Potashev <aspotashev@gmail.com>
2008-10-21 00:16:23 +0400

    directory types via next->d_type

--- backup/backup.c
+++ backup/backup.c
@@ -6,16 +6,20 @@
 #include <errno.h>
 
 #define err(format, args...) \
-	fprintf (stderr, "backup: " \
+	fprintf (stderr, "backup error: " \
 		format "%s%s\n", \
 		## args, \
 		errno ? "\n\terror type: " : "", \
 		errno ? strerror (errno) : "")
 
+#define info(format, args...) \
+	fprintf (stdout, "backup info: " \
+		format "\n", ## args)
+
 int backup_dir (const char *backup, const char *source)
 {
 	DIR *source_dir;
-	struct dirent *dp;
+	struct dirent *next;
 
 	source_dir = opendir (source);
 	if (source_dir == NULL) {
@@ -23,8 +27,16 @@ int backup_dir (const char *backup, const char *source)
 		return -1;
 	}
 
-	while ((dp = readdir (dir)) != NULL) {
-		...
+	while ((next = readdir (source_dir)) != NULL) {
+		switch (next->d_type) {
+		case DT_BLK:  info ("%s is a block device", next->d_name);     break;
+		case DT_CHR:  info ("%s is a character device", next->d_name); break;
+		case DT_FIFO: info ("%s is a fifo", next->d_name);             break;
+		case DT_LNK:  err ("FIXME: symbolic link backups are not implemented! %s", next->d_name); break;
+		case DT_REG:  err ("FIXME: reg. file %s", next->d_name); break;
+		case DT_SOCK: info ("%s is a socket", next->d_name);           break;
+		default: err ("unknown file type");
+		}
 	}
 
 	return 0;
