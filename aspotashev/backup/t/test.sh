4424f9e93fff8ecbcb6fb88b4e24b563a5b7fd35
Alexander Potashev <aspotashev@gmail.com>
2008-12-07 00:45:42 +0300

    test.sh

---
+++ aspotashev/backup/t/test.sh
@@ -0,0 +1,19 @@
+
+mkdir -p sandbox
+cd sandbox
+
+rm -rf temp
+
+mkdir temp
+ln -s file.txt temp/symlink
+echo "123" > temp/file.txt
+link temp/file.txt temp/hardlink
+
+mkdir -p temp.backup
+
+cd ..
+make
+cd -
+
+../../backup temp temp.backup
+
